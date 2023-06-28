38ff7652853bf336410a8eff8bd51fc9ceb06e6b
Tiago Gomes <tidgomes@gmail.com>
2023-06-28 10:49:20 -0300

    test: return an error in the body of the response for both email and name when one of them is missing

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -15,4 +15,18 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
+
+  test('should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'name',
+        password: 'pass123',
+        passwordConfirmation: 'pass123'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  })
 })
