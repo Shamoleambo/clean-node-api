6c741019a9fd2546a6fb0ce75ea5b04291091a4d
Tiago Gomes <tidgomes@gmail.com>
2023-07-10 12:13:02 -0300

    refactor: move mapper to helper function

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -7,9 +7,8 @@ export class AccountMongoRepository implements AddAccountRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = MongoHelper.getCollection('accounts')
     const { insertedId } = await accountCollection.insertOne(accountData)
-    const { _id, ...account } = await accountCollection.findOne({ _id: insertedId })
+    const account = await accountCollection.findOne({ _id: insertedId })
 
-    account.id = _id.toString()
-    return account as AccountModel
+    return MongoHelper.map(account)
   }
 }
