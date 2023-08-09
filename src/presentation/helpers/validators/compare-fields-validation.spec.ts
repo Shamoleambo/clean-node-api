6c7d27315b778de502364b411d5f26cc94a92ac1
Tiago Gomes <tidgomes@gmail.com>
2023-08-09 08:38:01 -0300

    refactor: move sut creation to a factory method

--- src/presentation/helpers/validators/compare-fields-validation.spec.ts
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -1,16 +1,18 @@
 import { InvalidParamError } from '../../errors'
 import { CompareFieldsValidation } from './compare-fields-validation'
 
+const makeSut = (): CompareFieldsValidation => new CompareFieldsValidation('field', 'fieldToCompare')
+
 describe('CompareFields Validation', () => {
   test('should return an InvalidParamError if validation fails', () => {
-    const sut = new CompareFieldsValidation('field', 'fieldToCompare')
+    const sut = makeSut()
     const error = sut.validate({ field: 'value_one', fieldToCompare: 'value_two' })
 
     expect(error).toEqual(new InvalidParamError('fieldToCompare'))
   })
 
   test('should not return if validation succeeds', () => {
-    const sut = new CompareFieldsValidation('field', 'fieldToCompare')
+    const sut = makeSut()
     const validate = jest.spyOn(sut, 'validate')
 
     expect(validate).not.toReturn()
