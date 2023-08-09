897f7f25f387add77f79522b5a1b81920ab7a265
Tiago Gomes <tidgomes@gmail.com>
2023-08-09 08:36:03 -0300

    test: ensure CompareFields Validation returns nothing if validation succeeds

--- src/presentation/helpers/validators/compare-fields-validation.spec.ts
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -3,9 +3,16 @@ import { CompareFieldsValidation } from './compare-fields-validation'
 
 describe('CompareFields Validation', () => {
   test('should return an InvalidParamError if validation fails', () => {
-    const sut = new CompareFieldsValidation('fieldOne', 'fieldTwo')
-    const error = sut.validate({ fieldOne: 'value_one', fieldTwo: 'value_two' })
+    const sut = new CompareFieldsValidation('field', 'fieldToCompare')
+    const error = sut.validate({ field: 'value_one', fieldToCompare: 'value_two' })
 
-    expect(error).toEqual(new InvalidParamError('fieldTwo'))
+    expect(error).toEqual(new InvalidParamError('fieldToCompare'))
+  })
+
+  test('should not return if validation succeeds', () => {
+    const sut = new CompareFieldsValidation('field', 'fieldToCompare')
+    const validate = jest.spyOn(sut, 'validate')
+
+    expect(validate).not.toReturn()
   })
 })
