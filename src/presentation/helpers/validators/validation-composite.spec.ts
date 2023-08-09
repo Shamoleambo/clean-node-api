bf290954cc2b436cff69482db6fb7f0547af330f
Tiago Gomes <tidgomes@gmail.com>
2023-08-09 08:58:36 -0300

    test: ensure ValidationCompiste does not returns if validation succeeds

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -31,4 +31,11 @@ describe('Validation Composite', () => {
 
     expect(error).toEqual(new MissingParamError('field'))
   })
+
+  test('should not return if validations succeed', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ field: 'any_value' })
+
+    expect(error).toBeFalsy()
+  })
 })
