c19bdc5b2865d3453a109d6c43a5fdb107bcebc1
Tiago Gomes <tidgomes@gmail.com>
2023-08-09 09:03:08 -0300

    refactor: make the factory method return an array of stubs

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -14,19 +14,19 @@ const makeValidationStub = (): Validation => {
 
 interface SutTypes {
   sut: ValidationComposite
-  validationStub: Validation
+  validationStubs: Validation[]
 }
 const makeSut = (): SutTypes => {
-  const validationStub = makeValidationStub()
-  const sut = new ValidationComposite([validationStub])
+  const validationStubs = [makeValidationStub(), makeValidationStub()]
+  const sut = new ValidationComposite(validationStubs)
 
-  return { sut, validationStub }
+  return { sut, validationStubs }
 }
 
 describe('Validation Composite', () => {
   test('should return an error if validation fails', () => {
-    const { sut, validationStub } = makeSut()
-    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('field'))
+    const { sut, validationStubs } = makeSut()
+    jest.spyOn(validationStubs[0], 'validate').mockReturnValueOnce(new MissingParamError('field'))
     const error = sut.validate({ field: 'any_value' })
 
     expect(error).toEqual(new MissingParamError('field'))
